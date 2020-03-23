88c9c0eb6dfeed298af892f96a3f097694e8bbef
juanpablodf08 <jpdefrutos@gmail.com>
2020-03-23 04:06:37 -0300

    first commit

---
+++ src/ar/edu/utn/frsf/died/prueba/ImprimirHolaMundo.java
@@ -0,0 +1,10 @@
+package ar.edu.utn.frsf.died.prueba;
+
+public class ImprimirHolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+	}
+
+}
